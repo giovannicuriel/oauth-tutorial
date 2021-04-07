14b13fe9b49ac8f09da4b6cb88c90e70c48bcc46
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2021-04-06 22:06:22 -0300

    initial application code

---
+++ OfficeStorage/src/main/kotlin/br/com/giovanni/office/storage/OfficeStorageApplication.kt
@@ -0,0 +1,11 @@
+package br.com.giovanni.office.storage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OfficeStorageApplication
+
+fun main(args: Array<String>) {
+	runApplication<OfficeStorageApplication>(*args)
+}
---
+++ OfficeStorage/src/test/kotlin/br/com/giovanni/office/storage/OfficeStorageApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.giovanni.office.storage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OfficeStorageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
